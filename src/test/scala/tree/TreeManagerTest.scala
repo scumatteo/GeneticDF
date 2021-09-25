e335c3dbed16b70908af98318fd08680bfb310e5
Matteo Scucchia <matteo.scucchia@studio.unibo.it>
2021-09-25 11:37:21 +0200

    Corrected two tests in TreeManagerTest

--- src/test/scala/tree/TreeManagerTest.scala
+++ src/test/scala/tree/TreeManagerTest.scala
@@ -7,11 +7,11 @@ class TreeManagerTest extends AnyFunSuite {
 
   val populationSize: Int = 50
 
-  test(s"The method createInitialPopulation should return a list of IE-Tree of size = populationSize"){
+  test(s"The method createInitialPopulation should return a list of IE-Tree of size equals to populationSize"){
     assert(TreeManager.createInitialPopulation(populationSize).size == populationSize)
   }
 
-  test(s"The method createRandomTree should create a random IE-Tree of depth <= maxDepth"){
+  test(s"The method createRandomTree should create a random IE-Tree of depth less or equal to maxDepth"){
     assert(TreeManager.createRandomTree().depth() <= GPManager.MAX_TREE_DEPTH)
   }
 
